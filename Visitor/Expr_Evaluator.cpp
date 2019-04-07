140bf358863d12f73ba3a57197a69330af485e88
Bryan McClain <bamcclain10@gmail.com>
2019-04-07 11:48:21 -0400

    Wrote Expr_Evaluator class

---
+++ Visitor/Expr_Evaluator.cpp
@@ -0,0 +1,121 @@
+// Honor Pledge:
+//
+// I pledge that I have neither given nor received any help
+// on this assignment
+#include "Expr_Evaluator.h"
+
+
+
+//
+// Reset the evaluator state
+//
+void Expr_Evaluator::reset_evaluator() {
+	this->stack_.clear();
+}
+
+
+
+//
+// Get the result of the last expression
+//
+int Expr_Evaluator::get_result() const {
+	return this->stack_.top();
+}
+
+
+
+//
+// Visit the number
+//
+void Expr_Evaluator::visit_number_node(Number_Node& node) {
+	this->stack_.push(node.get_value());
+}
+
+
+//
+// Visit the variable
+//
+void Expr_Evaluator::visit_variable_node(Variable_Node& node) {
+	this->stack_.push(node.get_value());
+}
+
+
+//
+// Visit the binary nodes
+//
+void Expr_Evaluator::visit_add_node(Add_Node& node) {
+	this->do_binary_node(node);
+}
+
+void Expr_Evaluator::visit_subtract_node(Subtract_Node& node) {
+	this->do_binary_node(node);
+}
+
+void Expr_Evaluator::visit_multiply_node(Multiply_Node& node) {
+	this->do_binary_node(node);
+}
+
+void Expr_Evaluator::visit_divide_node(Divide_Node& node) {
+	this->do_binary_node(node);
+}
+
+void Expr_Evaluator::visit_modulus_node(Modulus_Node& node) {
+	this->do_binary_node(node);
+}
+
+
+//
+// Visit the unary nodes
+//
+void Expr_Evaluator::visit_negate_node(Negate_Node& node) {
+	this->do_unary_node(node);
+}
+
+void Expr_Evaluator::visit_square_root_node(Square_Root_Node& node) {
+	this->do_unary_node(node);
+}
+
+
+
+
+//
+// Process a binary node
+//
+void Expr_Evaluator::do_binary_node(Binary_Node& node) {
+
+	//Evaluate the left and right nodes first
+	node.get_left_expression()->accept(*this);
+	node.get_right_expression()->accept(*this);
+
+	// Pop values off stack in reverse order, then push
+	int first, second;
+	second = this->stack_.pop();
+	first = this->stack_.pop();
+
+	// Run the stored binary method
+	int result = node.do_operation(first, second);
+
+	// Push the result back onto the stack
+	this->stack_.push(result);
+}
+
+
+
+
+//
+// Process a unary node
+//
+void Expr_Evaluator::do_unary_node(Unary_Node& node) {
+
+	//Evaluate the child expression first
+	node.get_child_expression()->accept(*this);
+
+	//Pop 1 number off the stack
+	int val = this->stack_.pop();
+
+	//Run the unary operation
+	val = node.do_operation(val);
+
+	//Push the result back onto the stack
+	this->stack_.push(val);
+}
---
+++ Visitor/Expr_Evaluator.h
@@ -0,0 +1,64 @@
+//==============================================================================
+/**
+ * @file       Expr_Evaluator.h
+ *
+ * Honor Pledge:
+ *
+ * I pledge that I have neither given nor received any help
+ * on this assignment.
+ */
+//==============================================================================
+
+#ifndef EXPR_EVALUATOR_HEADER
+#define EXPR_EVALUATOR_HEADER
+
+#include <Expr_Node_Visitor.h>
+#include <Stack.h>
+
+
+/**
+ * @class Expr_Evaluator
+ *
+ * Evaluate the expression in the Expr_Node hierarchy using a stack
+ * and the visitor pattern;
+ */
+class Expr_Evaluator: public Expr_Node_Visitor {
+
+	/// Call this method to clear the internal state before
+	///  attempting to evaluate a new expression
+	void reset_evaluator();
+
+	/**
+	 * Get the result of the last evaluated expression
+	 * @return		Expression result
+	 * @throws		empty_exception
+	 */
+	int get_result() const;
+
+	/// All of the concrete node types that can be visited
+	void visit_number_node(Number_Node& node);
+	void visit_variable_node(Variable_Node& node);
+	void visit_add_node(Add_Node& node);
+	void visit_subtract_node(Subtract_Node& node);
+	void visit_multiply_node(Multiply_Node& node);
+	void visit_divide_node(Divide_Node& node);
+	void visit_modulus_node(Modulus_Node& node);
+	void visit_negate_node(Negate_Node& node);
+	void visit_square_root_node(Square_Root_Node& node);
+
+
+private:
+
+	/// Treat all unary nodes uniformly when processing
+	void do_unary_node(Unary_Node& node);
+
+	/// Treat all binary nodes uniformly when processing
+	void do_binary_node(Binary_Node& node);
+
+
+	/// Internal stack used to evaluate the expression
+	Stack<int> stack_;
+
+}; 
+
+#endif	/* Expression Evaluator Header Included */
