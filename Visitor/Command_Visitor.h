fdf553b1f2045fcaea97f164510db92738208126
Bryan McClain <bamcclain10@gmail.com>
2019-12-10 20:11:39 -0500

    Fixed problem with visitor class

--- Visitor/Command_Visitor.h
+++ Visitor/Command_Visitor.h
@@ -12,7 +12,7 @@
 #ifndef COMMAND_VISITOR_HEADER
 #define COMMAND_VISITOR_HEADER
 
-//Forward declare all of the concrete class types
+// Forward declare all of the concrete class types
 class Number_Command;
 class Variable_Command;
 class Add_Command;
@@ -23,24 +23,22 @@ class Modulus_Command;
 class Negate_Command;
 class Square_Root_Command;
 
-
 class Command_Visitor {
 
 public:
-	/// Destructor
-	virtual ~Command_Visitor() = default;
+  /// Destructor
+  virtual ~Command_Visitor() = default;
 
-	/// All of the concrete command types that can be visited
-	virtual void visit_number_command(const Number_Command& command) = 0;
-	virtual void visit_variable_command(const Variable_Command& command) = 0;
-	virtual void visit_add_command(const Add_Command& command) = 0;
-	virtual void visit_subtract_command(const Subtract_Command& command) = 0;
-	virtual void visit_multiply_command(const Multiply_Command& command) = 0;
-	virtual void visit_divide_command(const Divide_Command& command) = 0;
-	virtual void visit_modulus_command(const Modulus_Command& command) = 0;
-	virtual void visit_negate_command(const Negate_Command& command) = 0;
-	virtual void visit_square_root_command(const Square_Root_Command& command) = 0;
+  /// All of the concrete command types that can be visited
+  virtual void visit_number_command(Number_Command &command) = 0;
+  virtual void visit_variable_command(Variable_Command &command) = 0;
+  virtual void visit_add_command(Add_Command &command) = 0;
+  virtual void visit_subtract_command(Subtract_Command &command) = 0;
+  virtual void visit_multiply_command(Multiply_Command &command) = 0;
+  virtual void visit_divide_command(Divide_Command &command) = 0;
+  virtual void visit_modulus_command(Modulus_Command &command) = 0;
+  virtual void visit_negate_command(Negate_Command &command) = 0;
+  virtual void visit_square_root_command(Square_Root_Command &command) = 0;
 };
 
-
-#endif	/* Command Visitor Header Included */
+#endif /* Command Visitor Header Included */
--- Visitor/Expr_Node_Visitor.h
+++ Visitor/Expr_Node_Visitor.h
@@ -12,7 +12,7 @@
 #ifndef EXPRESSION_NODE_VISITOR_HEADER
 #define EXPRESSION_NODE_VISITOR_HEADER
 
-//Forward declare all of the concreate types
+// Forward declare all of the concreate types
 class Number_Node;
 class Variable_Node;
 class Add_Node;
@@ -23,24 +23,22 @@ class Modulus_Node;
 class Negate_Node;
 class Square_Root_Node;
 
-
 class Expr_Node_Visitor {
 
 public:
-	/// Destructor
-	virtual ~Expr_Node_Visitor() = default;
+  /// Destructor
+  virtual ~Expr_Node_Visitor() = default;
 
-	/// All of the concrete node types that can be visited
-	virtual void visit_number_node(const Number_Node& node) = 0;
-	virtual void visit_variable_node(const Variable_Node& node) = 0;
-	virtual void visit_add_node(const Add_Node& node) = 0;
-	virtual void visit_subtract_node(const Subtract_Node& node) = 0;
-	virtual void visit_multiply_node(const Multiply_Node& node) = 0;
-	virtual void visit_divide_node(const Divide_Node& node) = 0;
-	virtual void visit_modulus_node(const Modulus_Node& node) = 0;
-	virtual void visit_negate_node(const Negate_Node& node) = 0;
-	virtual void visit_square_root_node(const Square_Root_Node& node) = 0;
+  /// All of the concrete node types that can be visited
+  virtual void visit_number_node(Number_Node &node) = 0;
+  virtual void visit_variable_node(Variable_Node &node) = 0;
+  virtual void visit_add_node(Add_Node &node) = 0;
+  virtual void visit_subtract_node(Subtract_Node &node) = 0;
+  virtual void visit_multiply_node(Multiply_Node &node) = 0;
+  virtual void visit_divide_node(Divide_Node &node) = 0;
+  virtual void visit_modulus_node(Modulus_Node &node) = 0;
+  virtual void visit_negate_node(Negate_Node &node) = 0;
+  virtual void visit_square_root_node(Square_Root_Node &node) = 0;
 };
 
-
-#endif	/* Expression Node Visitor Header Included */
+#endif /* Expression Node Visitor Header Included */
